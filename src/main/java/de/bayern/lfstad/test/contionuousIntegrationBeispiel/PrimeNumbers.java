86f6c8add9e3f3911d29125a9aefe7041d86e81f
Tobias Kremer (LfStaD) <tobias.kremer@lfstad.bayern.de>
2013-07-16 13:56:49 +0200

    Schwenk auf etwas, das vieleicht sogar als dauerhafte Implementierung taugt.

--- src/main/java/de/bayern/lfstad/test/contionuousIntegrationBeispiel/PrimeNumbers.java
+++ src/main/java/de/bayern/lfstad/test/contionuousIntegrationBeispiel/PrimeNumbers.java
@@ -1,12 +1,18 @@
 package de.bayern.lfstad.test.contionuousIntegrationBeispiel;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Erlaubt es eine Zahl in ihre Prrim-Faktoren zu zerlegen.
  */
 public class PrimeNumbers {
+
+    /**
+     * Enthält die bekannten Prim-Faktoren.
+     */
+    private List<Integer> primeNumbers = Arrays.asList(2, 3, 5, 7);
 
     /**
      * Führt die Zerlegung des übergebenen {@code values} in seine Prim-Faktoren durch.
@@ -18,6 +24,27 @@ public class PrimeNumbers {
      *         aufsteigend nach ihrer Größe geliefert.
      */
     public List<Integer> split(final int value) {
-        return Arrays.asList(2);
+        if(value == 1) {
+            return Arrays.asList(1);
+        }
+
+        final List<Integer> result = new LinkedList<>();
+
+        int tempValue = value;
+
+        OUTER_LOOP: while(tempValue != 1) {
+            for(final Integer primeNumber : primeNumbers) {
+                if(tempValue % primeNumber.intValue() == 0) {
+                    result.add(primeNumber);
+                    tempValue = tempValue / primeNumber.intValue();
+
+                    continue OUTER_LOOP;
+                }
+            }
+
+            throw new RuntimeException("Endlosschleife!");
+        }
+
+        return result;
     }
 }
--- src/test/java/de/bayern/lfstad/test/contionuousIntegrationBeispiel/PrimeNumbersTest.java
+++ src/test/java/de/bayern/lfstad/test/contionuousIntegrationBeispiel/PrimeNumbersTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 /**
  * Testcases für die Zerlegung einer Zahl in Prim-Faktoren.
  */
+@SuppressWarnings("javadoc")
 public class PrimeNumbersTest {
 
     /**
@@ -17,7 +18,47 @@ public class PrimeNumbersTest {
     private PrimeNumbers primeNumbers = new PrimeNumbers();
 
     @Test
+    public void split1() {
+        assertEquals(Arrays.asList(1), primeNumbers.split(1));
+    }
+
+    @Test
     public void split2() {
         assertEquals(Arrays.asList(2), primeNumbers.split(2));
+    }
+
+    @Test
+    public void split3() {
+        assertEquals(Arrays.asList(3), primeNumbers.split(3));
+    }
+
+    @Test
+    public void split4() {
+        assertEquals(Arrays.asList(2, 2), primeNumbers.split(2 * 2));
+    }
+
+    @Test
+    public void split5() {
+        assertEquals(Arrays.asList(5), primeNumbers.split(5));
+    }
+
+    @Test
+    public void split6() {
+        assertEquals(Arrays.asList(2, 3), primeNumbers.split(2 * 3));
+    }
+
+    @Test
+    public void split7() {
+        assertEquals(Arrays.asList(7), primeNumbers.split(7));
+    }
+
+    @Test
+    public void split8() {
+        assertEquals(Arrays.asList(2, 2, 2), primeNumbers.split(2 * 2 * 2));
+    }
+
+    @Test
+    public void split9() {
+        assertEquals(Arrays.asList(3, 3), primeNumbers.split(3 * 3));
     }
 }
